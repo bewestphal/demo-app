5a2d64fe4a09f7df26d34d8f89587bc9258c6493
Brian Westphal <coding@brianwestphal.com>
2023-06-20 14:32:02 -0700

    fix: another change

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("bewestphal is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("bewestphal another change is Awesome!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
